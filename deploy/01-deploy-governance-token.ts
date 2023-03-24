9a52b228412fc328b61973291db3794222bb016b
Otaiki1 <otaikisadiq2804@gmail.com>
2023-03-24 16:17:31 +0100

    setup deploy script for governance token

--- deploy/01-deploy-governance-token.ts
+++ deploy/01-deploy-governance-token.ts
@@ -4,7 +4,21 @@ import { DeployFunction } from "hardhat-deploy/types";
 const deployGovernanceToken: DeployFunction = async (
   hre: HardhatRuntimeEnvironment
 ) => {
-  console.log("WHOOOO WHOOOO DAO WOW");
+  const {getNamedAccounts, deployments, network} = hre;
+  const {deployer} = await getNamedAccounts();
+  const{deploy, log} = deployments;
+
+  log("DEPLOYING GOVERNANCE TOKEN...................");
+
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args:[],
+    log: true,
+    //waitConfirmations
+  });
+
+  log("01 ---- DEPLOYED GOVERNANCE TOKEN AT ....", governanceToken.address)
 };
 
 export default deployGovernanceToken;
+deployGovernanceToken.tags = ["all", "governanceToken"];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,35 @@
 import "@typechain/hardhat";
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/config";
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity:{
+const config: HardhatUserConfig = {
+  solidity: {
     version: "0.8.18",
-    settings:{
+    settings: {
       optimizer: {
         enabled: true,
-        runs: 100
-      }
-    }
-  } 
+        runs: 100,
+      },
+    },
+  },
+  //networks
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0, //index
+    },
+  },
 };
+
+export default config;
