4688a523fc10e244e188bff18471b97123ade6f9
Otaiki1 <otaikisadiq2804@gmail.com>
2023-03-28 04:52:01 +0100

    refac: prettier fix deploy governance token

--- deploy/01-deploy-governance-token.ts
+++ deploy/01-deploy-governance-token.ts
@@ -5,36 +5,43 @@ import { ethers } from "hardhat";
 const deployGovernanceToken: DeployFunction = async (
   hre: HardhatRuntimeEnvironment
 ) => {
-  const {getNamedAccounts, deployments, network} = hre;
-  const {deployer} = await getNamedAccounts();
-  const{deploy, log} = deployments;
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy, log } = deployments;
 
   log("DEPLOYING GOVERNANCE TOKEN...................");
 
   const governanceToken = await deploy("GovernanceToken", {
     from: deployer,
-    args:[],
+    args: [],
     log: true,
     //waitConfirmations
   });
 
-  log("01 ---- DEPLOYED GOVERNANCE TOKEN AT ....", governanceToken.address)
+  log("01 ---- DEPLOYED GOVERNANCE TOKEN AT ....", governanceToken.address);
 
-  //DELEGATE 
+  //DELEGATE
   await delegate(governanceToken.address, deployer);
-  log("---------01-DELEGATED----------")
+  log("---------01-DELEGATED----------");
 };
 
 export default deployGovernanceToken;
 deployGovernanceToken.tags = ["all", "governanceToken"];
 
-const delegate = async(governanceTokenAddress: string, delegatedAccount: string) => {
+const delegate = async (
+  governanceTokenAddress: string,
+  delegatedAccount: string
+) => {
+  const governanceToken = await ethers.getContractAt(
+    "GovernanceToken",
+    governanceTokenAddress
+  );
 
-    const governanceToken  = await ethers.getContractAt("GovernanceToken", governanceTokenAddress);
+  const txResponse = await governanceToken.delegate(delegatedAccount);
 
-    const txResponse = await governanceToken.delegate(delegatedAccount);
+  await txResponse.wait(1);
 
-    await txResponse.wait(1);
-
-    console.log(`checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`);
-}
+  console.log(
+    `checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`
+  );
+};
