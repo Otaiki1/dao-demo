2238d64a5c10abed02f474604bec0f9969bcf4b1
Otaiki1 <otaikisadiq2804@gmail.com>
2023-04-06 11:53:07 +0100

    refac: change voting period

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -1,5 +1,5 @@
 export const VOTING_DELAY = 1; // 1 block
-export const VOTING_PERIOD = 50400; // 1 weeek in  block
+export const VOTING_PERIOD = 5; // 1 weeek in  block
 export const QUORUM_PERCENTAGE = 4; // 4 percent
 
 export const MIN_DELAY = 3600;
